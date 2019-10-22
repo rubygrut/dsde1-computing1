70702df564b65e7b648c7d7e527b068d4a22a4dd
Ruby Grut <ruby.grut19@imperial.ac.uk>
2019-10-22 19:14:04 +0100

    changes week3

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -1,43 +1,42 @@
-a=int(input('Enter a numero:'))
-b=int(input('Give me another numero:'))
-def inc_return(a):
-    product = a+1
-    return product
-print('Your first number plus one is:' + str(inc_return(a)))
+def inc(a):
+    float(a)    
+    a=a+1
+    print(a)
+
+def inc_return(d):
+    float(d)
+    d=d+1
+    return(d)
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 
 def sum(a, b):
-    sum = a+b
-    return sum
-print('Your first number plus your second number is:'+str(sum(a, b)))
+    int(a)
+    int(b)
+    c= a +b
+    return(c)
 
 # write a function that takes two
 # numbers, adds them together using 
 # sum() and then increments the sum
 # using inc_return
 
-def inc_return(a, b):
-    inc_return = a+b+1
-    return inc_return
-print('Your first number plus the second incremented by 1 is:' + str(inc_return(a, b)))
+def sum_inc(a, b):
+    int(a)
+    int(b)
+    c=a+b
+    d=inc_return(c)
+    return(d)
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 
-c=int(input('Ay up give me an even number:'))
-def if_even(c):
-    if c%2==0:
-        return True
-    else:
-        return False
-if if_even(c)==True:
-    print('Well done you')
-else:
-    print('Bad try')
+def is_even(a):
+    b=(a%2==0)
+    return(b)
 
 # create for loop that takes a string
 # and an integer and returns a new string 
@@ -46,9 +45,7 @@ else:
 # e.g. string_repeat('ho', 3) returns
 # 'hohoho'
 
-d = str(input('Tell me a short phrase:'))
-e = int(input('Give me a number less than 10:'))
-def string_repeat(d, e):
-    product = d * e
-    return product
-print('Your phrase repeated as many times as your number is:' + string_repeat(d, e))
+def string_repeat(phrase, repeat):
+    str(phrase)
+    int(repeat)
+    return(phrase * repeat)
